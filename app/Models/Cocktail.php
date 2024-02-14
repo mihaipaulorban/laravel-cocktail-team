ca969d512e52abcb108a29fd1cd1cafdefd3daec
AlbertoMessineo <albevorox@gmail.com>
2024-02-14 15:58:29 +0100

    feat: pivot table & manytomanyy relationship

--- app/Models/Cocktail.php
+++ app/Models/Cocktail.php
@@ -5,9 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Cocktail extends Model
+
 {
     use HasFactory;
-
     protected $guarded = [];
+    public function ingredients() 
+    {
+        return $this->belongsToMany(Ingredient::class);
+    }
 }
